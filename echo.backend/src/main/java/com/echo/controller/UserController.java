f91bf320910093df743144020cdf20a43686e217
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-05-22 15:13:26 +0200

    Data registrazione settata

--- echo.backend/src/main/java/com/echo/controller/UserController.java
+++ echo.backend/src/main/java/com/echo/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.echo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.echo.dto.UserDto;
 import com.echo.service.UserService;
 
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 	
 	@Autowired
 	private UserService uService;
+
+	@GetMapping("/find")
+	public UserDto findbyId (@RequestParam("id") Long id) {
+		
+		return uService.findByIdService(id);
+	}
 }

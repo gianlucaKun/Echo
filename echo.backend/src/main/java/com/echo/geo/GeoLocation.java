b4351d3b3134c76ce47270a5f3d6ee8abd8d66f5
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-05-22 16:00:35 +0200

    Aggiunto localizzazione funzionante

---
+++ echo.backend/src/main/java/com/echo/geo/GeoLocation.java
@@ -0,0 +1,12 @@
+package com.echo.geo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeoLocation {
+    private String city;
+}
---
+++ echo.backend/src/main/java/com/echo/geo/GeoLocationService.java
@@ -0,0 +1,17 @@
+package com.echo.geo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class GeoLocationService {
+
+//	private final String API_KEY = "fca8f3a6a175f9";
+//	private final String API_URL = "https://ipinfo.io/";
+//
+//    public GeoLocation getGeoLocation(String ipAddress) {
+//        String url = API_URL + ipAddress + "?access_key=" + API_KEY;
+//        RestTemplate restTemplate = new RestTemplate();
+//        return restTemplate.getForObject(url, GeoLocation.class);
+//    }
+}

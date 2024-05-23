f3c8600e9e020d89bf7a65beb2fad89f188986f8
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-05-23 17:45:12 +0200

    Aggiunto pw criptate al db

--- echo.backend/src/main/java/com/echo/service/UserService.java
+++ echo.backend/src/main/java/com/echo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.echo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.echo.dto.UserDto;
@@ -8,57 +9,62 @@ import com.echo.entity.User;
 import com.echo.mapper.UserMapper;
 import com.echo.repository.UserRepository;
 
-
 @Service
 public class UserService {
 
-	
 	@Autowired
 	private UserRepository uRepository;
-	
+
 	@Autowired
 	private UserMapper uMapper;
+
+	@Autowired
+	private PasswordEncoder pwEncoder;
 
 	public boolean loginService(User user) {
 
 		User findUser = uRepository.findByUsername(user.getUsername());
-		
-		if (findUser != null & findUser.getPassword().equals(user.getPassword())) {
-			
-			return true;
-		} else {
-			return false;
+
+		if (findUser != null) {
+
+			return pwEncoder.matches(user.getPassword(), findUser.getPassword());
 		}
+		return false;
 	}
 
 	public UserDto createUserService(User user) {
+		String encodedPassword = pwEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 		User saved = uRepository.save(user);
 		return uMapper.toDto(saved);
 	}
 
 	public UserDto findByIdService(Long id) {
-		
+
 		User finded = uRepository.findById(id).orElse(null);
-		
-		if (finded != null ) {
-			return uMapper.toDto(finded);	
+
+		if (finded != null) {
+			return uMapper.toDto(finded);
 		} else {
 			return null;
 		}
 	}
-	
+
 	public boolean usernameIsPresent(String username) {
-		if(uRepository.findByUsername(username) != null) {
+		if (uRepository.findByUsername(username) != null) {
 			return true;
-		} else return false;
+		} else
+			return false;
 	}
-	
+
 	public boolean emailIsPresent(String email) {
-		
+
 		User finded = uRepository.findByEmail(email);
-		if(finded != null) {
-			System.out.println("è gia presente un utente con questa mail: " +finded.getEmail() + " utente: " + finded.getName());
+		if (finded != null) {
+			System.out.println(
+					"è gia presente un utente con questa mail: " + finded.getEmail() + " utente: " + finded.getName());
 			return true;
-		} else return false;
+		} else
+			return false;
 	}
 }

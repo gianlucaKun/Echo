dc68610c6a1bcc8caa45d28627453fee966d093e
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-05-23 17:04:55 +0200

    initialize Album, Artist, Sing entity

---
+++ echo.backend/src/main/java/com/echo/entity/Album.java
@@ -0,0 +1,24 @@
+package com.echo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Album {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+
+}
